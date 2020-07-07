09feaff33a023612ed49bd3cc74605b6699939f3
Michael Schnabel <michael-schnabel@gmx.net>
2020-07-07 23:27:59 +0200

    added vertex material node group

--- io_mesh_w3d/__init__.py
+++ io_mesh_w3d/__init__.py
@@ -416,6 +416,8 @@ CLASSES = (
     BONE_PROPERTIES_PANEL_PT_w3d,
     MATERIAL_PROPERTIES_PANEL_PT_w3d)
 
+
+from io_mesh_w3d.common.shading.vertex_material_group import VertexMaterialGroup
 from io_mesh_w3d.common.utils.node_group_creator import NodeGroupCreator
 
 
@@ -428,6 +430,8 @@ def create_node_groups():
             continue
         NodeGroupCreator().create(directory, file)
 
+    VertexMaterialGroup.register()
+
 
 def remove_node_groups():
     dirname = os.path.dirname(__file__)
@@ -437,6 +441,8 @@ def remove_node_groups():
         if not file.endswith(".xml"):
             continue
         NodeGroupCreator().unregister(directory, file)
+
+    VertexMaterialGroup.unregister()
 
 
 from io_mesh_w3d.common.shading.node_socket_enum import NodeSocketInterfaceEnum
